11b4c1353f680b5bbebbcf07edd98da364904d50
sionesmith <smithsione.s91@gmail.com>
2024-11-20 13:56:14 +1300

    step2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from './Header';
 
-function App() {
+function App () {
   return (
     <div className="App">
-      {/* Your components will go here */}
+      <Header/>
     </div>
   );
 }
